77a5c4f427697baf3144a2a9ee15b12c83720616
Simon Wong <thatonesimon@gmail.com>
2017-02-08 22:13:50 -0800

    added stuff to events
    cause michael

--- app/src/main/java/com/marioinfante/mytinerary/Event.java
+++ app/src/main/java/com/marioinfante/mytinerary/Event.java
@@ -6,23 +6,32 @@ package com.marioinfante.mytinerary;
 
 public class Event {
 
+    // each event needs to have these
     private Date date;
     private int time;
     private String title;
+
+    // each event can have this
     private String location;
+    private String description;
 
     Event(Date date, int time, String title){
         this.date = date;
         this.time = time;
         this.title = title;
+        this.location = "";
+        this.description = "";
     }
 
     public void setLocation(String loc){
         this.location = loc;
     }
 
-    public String toString(){
+    public void setDescription(String des){
+        this.description = des;
+    }
 
+    public String toString(){
         return "STUB";
     }
 
